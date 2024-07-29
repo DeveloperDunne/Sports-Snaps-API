292cb79ea4a56edb74196a3e8bc22df3e601f67c
Carl Dunne <carl.m.dunne@hotmail.com>
2024-07-29 10:26:53 +0000

    Added serializers to categories

---
+++ categories/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'created_at']
+
+
+class CategoryDetailSerializer(CategorySerializer):
+        category_id = serializers.ReadOnlyField(source='id')
+
+    
+        class Meta:
+            model = Category
+            fields = ['id', 'name', 'created_at', 'category_id']
--- categories/views.py
+++ categories/views.py
@@ -2,13 +2,16 @@ from django_filters.rest_framework import DjangoFilterBackend
 from ss_api.permissions import IsOwnerOrReadOnly
 from rest_framework import generics, permissions
 from .models import Categories
+from .serializers import CategorySerializer, CategoryDetailSerializer
 
 class CategoryList(generics.ListAPIView):
     queryset = Categories.objects.all()
+    serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name']
 
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categories.objects.all()
+    serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
