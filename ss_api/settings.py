4be1a703f42e9694f897be41861f944417fece82
Carl Dunne <carl.m.dunne@hotmail.com>
2024-07-30 10:32:53 +0000

    Amended Settings

--- ss_api/settings.py
+++ ss_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -59,7 +60,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost',
+ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOST'),
+                'localhost',
                  'sportssnaps-api-4f584f2a4ad4.herokuapp.com',
                  '127.0.0.1',
                  '8000-developerdu-sportssnaps-hqp090d211n.ws-eu115.gitpod.io',
@@ -116,10 +118,13 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
-else:
-     CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
-     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
