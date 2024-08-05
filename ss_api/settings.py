07a5260665f2f7c39b504fc2e519dd3e20de9a40
Carl Dunne <carl.m.dunne@hotmail.com>
2024-08-05 20:15:50 +0000

    Amended settings

--- ss_api/settings.py
+++ ss_api/settings.py
@@ -153,17 +153,17 @@ WSGI_APPLICATION = 'ss_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if 'DEV' in os.environ:
-     DATABASES = {
-         'default': {
-             'ENGINE': 'django.db.backends.sqlite3',
-             'NAME': BASE_DIR / 'db.sqlite3',
-         }
-     }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
-     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
      
 
 
