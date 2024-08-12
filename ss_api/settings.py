11eee852bd255d10663a0a4154cc07381fceeed0
Carl Dunne <carl.m.dunne@hotmail.com>
2024-08-12 18:00:31 +0000

    Amended settings

--- ss_api/settings.py
+++ ss_api/settings.py
@@ -106,7 +106,6 @@ INSTALLED_APPS = [
 SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
